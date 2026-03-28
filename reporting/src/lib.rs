2e6c4d629279154fb4d45ed162b120bb293b07df
RemmyAcee <remmydesigns38@gmail.com>
2026-03-28 01:04:53 +0100

    fix

--- reporting/src/lib.rs
+++ reporting/src/lib.rs
@@ -307,6 +307,14 @@ pub struct InsurancePolicy {
     pub tags: Vec<soroban_sdk::String>,
 }
 
+#[contracttype]
+#[derive(Clone)]
+pub struct PolicyPage {
+    pub items: Vec<InsurancePolicy>,
+    pub next_cursor: u32,
+    pub count: u32,
+}
+
 #[contract]
 pub struct ReportingContract;
 
