7c2bdffd57c302969183e8e2f7f247dd6b591315
RemmyAcee <remmydesigns38@gmail.com>
2026-03-28 01:18:50 +0100

    fix

--- testutils/src/lib.rs
+++ testutils/src/lib.rs
@@ -26,7 +26,6 @@ pub fn generate_test_address(env: &Env) -> Address {
 
 #[macro_export]
 macro_rules! setup_test_env {
-<<<<<<< feature/reporting-archive-retention-policy
     ($env:ident, $contract:ident, $client:ident, $owner:ident, $client_type:ident) => {
         let $env = Env::default();
         $env.mock_all_auths();
@@ -38,12 +37,6 @@ macro_rules! setup_test_env {
         let $env = Env::default();
         $env.mock_all_auths();
         let contract_id = $env.register_contract(None, $contract);
-=======
-    ($env:ident, $contract:ident, $client_struct:ident, $client:ident, $owner:ident) => {
-        let $env = Env::default();
-        $env.mock_all_auths();
-        let contract_id = $env.register_contract(None, $contract);
->>>>>>> main
         let $client = $client_struct::new(&$env, &contract_id);
         let $owner = $crate::generate_test_address(&$env);
     };
